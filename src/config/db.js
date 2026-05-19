12a5ab2c78b6225bbb9b48b4395103b99a74f615
alloyking1 <alloyking1@gmail.com>
2026-05-19 19:13:52 +0100

    updated APP_NAME to appName

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
-const DATABASE_NAME = "devreal-node-auth-db";
-const APP_NAME = "devreal-tutorial-nodejs-authentication-geeksforgeeks";
+const databaseName = "devreal-node-auth-db";
+const appName = "devreal-tutorial-nodejs-authentication-geeksforgeeks";
 
 const connectDB = async () => {
   try {
-    const mongoUri = `${process.env.MONGO_URI_BASE}/${DATABASE_NAME}?retryWrites=true&w=majority&appName=${APP_NAME}`;
+    const mongoUri = `${process.env.MONGO_URI_BASE}/${databaseName}?retryWrites=true&w=majority&appName=${appName}`;
 
     await mongoose.connect(mongoUri);
 
     console.log("MongoDB connected");
-    console.log(`Database: ${DATABASE_NAME}`);
-    console.log(`App Name: ${APP_NAME}`);
+    console.log(`Database: ${databaseName}`);
+    console.log(`App Name: ${appName}`);
   } catch (error) {
     console.error("MongoDB connection failed:");
     console.error(error.message);
